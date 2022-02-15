17b55d3abaa9484b92d5ec333213f53032ae802f
jamieastley <6836556+jamieastley@users.noreply.github.com>
2022-02-15 18:40:55 +1100

    remove `drift` in favour of `Isar`

--- lib/infrastructure/database/platform/unsupported.dart
+++
@@ -1,9 +0,0 @@
-import 'package:standup_games/infrastructure/database/app_database.dart';
-
-/// DO NOT import this directly, only import and init via `shared_interface.dart`.
-Future<AppDatabase> constructDb({
-  String? dbName,
-  bool logStatements = false,
-}) {
-  throw 'Platform not supported';
-}
