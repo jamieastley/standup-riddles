2b422bb572e1971d0c91a2010c994a030bb28e72
jamieastley <6836556+jamieastley@users.noreply.github.com>
2022-02-16 19:45:31 +1100

    squash with injector

--- lib/core/injector.dart
+++ lib/core/injector.dart
@@ -1,23 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:isar/isar.dart';
-import 'package:standup_games/infrastructure/isar_database/models/content.dart';
-import 'package:standup_games/infrastructure/isar_database/models/picker.dart';
-import 'package:standup_games/repository/content_repository.dart';
-import 'package:standup_games/repository/picker_repository.dart';
+
+import '../infrastructure/isar_database/models/content.dart';
+import '../infrastructure/isar_database/models/picker.dart';
+import '../repository/content_repository.dart';
+import '../repository/picker_repository.dart';
 
 class Injector {
   Future<void> init() async {
     WidgetsFlutterBinding.ensureInitialized();
-    GetIt.I.registerSingletonAsync<Isar>(() async {
-      return await Isar.open(
-        schemas: [
-          ContentSchema,
-          PickerSchema,
-        ],
-        directory: '',
-      );
-    });
+    GetIt.I.registerSingletonAsync<Isar>(() async => Isar.open(
+          schemas: [
+            ContentSchema,
+            PickerSchema,
+          ],
+          directory: '',
+          // directory: dir.path,
+          // inspector: true,
+        ));
 
     GetIt.I.registerSingletonWithDependencies<ContentRepository>(
       () => ContentRepository(),
