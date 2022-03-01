0b8468a13851e3331bed5f7a6af1214b2e3cb338
jamieastley <6836556+jamieastley@users.noreply.github.com>
2022-03-02 09:03:10 +1100

    set flexible maxWidth on picker settings page

---
+++ lib/core/consts.dart
@@ -0,0 +1,10 @@
+class Dimensions {
+  static const maxWidth = 650.0;
+
+  static const spacing = 8.0;
+  static const spacing2x = spacing * 2;
+  static const spacing3x = spacing * 3;
+  static const spacing4x = spacing * 4;
+  static const spacing5x = spacing * 5;
+  static const spacing6x = spacing * 6;
+}
